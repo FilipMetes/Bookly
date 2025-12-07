e643b10e2af1866d8d0f775015fd605e597bd917
M3T3S4K <filipmetes04@gmail.com>
2025-12-07 14:35:45 +0100

    Checkpoint 2

--- App/Views/Auth/logout.view.php
+++ App/Views/Auth/logout.view.php
@@ -9,9 +9,9 @@ $view->setLayout('auth');
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-9 col-md-7 col-lg-5">
-            You have logged out. <br>
-            Again <a href="<?= App\Configuration::LOGIN_URL ?>">log in</a> or return <a
-                    href="<?= $link->url("home.index") ?>">back</a> to the home page?
+            Boli ste odhlásený. <br>
+            Znova sa <a href="<?= App\Configuration::LOGIN_URL ?>">prihlásiť</a> alebo vrátiť <a
+                    href="<?= $link->url("home.index") ?>">späť</a> na domovskú stránku?
         </div>
     </div>
 </div>
--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -13,32 +13,6 @@
             </p>
         </div>
     </div>
-
-    <!-- Featured categories or recommendations -->
-    <div class="row mb-5 text-center">
-        <div class="col-md-4 mb-4">
-            <div class="card feature-card h-100 p-3">
-                <img src="<?= $link->asset('images/genres.png') ?>" alt="Žánre" class="feature-img mb-3">
-                <h5>Žánre</h5>
-                <p>Prehľadajte knihy podľa žánrov a nájdite nové obľúbené tituly.</p>
-            </div>
-        </div>
-        <div class="col-md-4 mb-4">
-            <div class="card feature-card h-100 p-3">
-                <img src="<?= $link->asset('images/authors.png') ?>" alt="Autori" class="feature-img mb-3">
-                <h5>Autori</h5>
-                <p>Objavte knihy od vašich obľúbených autorov a spoznajte nových spisovateľov.</p>
-            </div>
-        </div>
-        <div class="col-md-4 mb-4">
-            <div class="card feature-card h-100 p-3">
-                <img src="<?= $link->asset('images/recommend.png') ?>" alt="Odporúčané" class="feature-img mb-3">
-                <h5>Odporúčané</h5>
-                <p>Inšpirujte sa odporúčaniami a objavte bestsellery a skryté perly.</p>
-            </div>
-        </div>
-    </div>
-
     <!-- About section -->
     <div class="row mb-5">
         <div class="col text-center">
